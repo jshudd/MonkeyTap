7f705c81bfa030d2257c95f3b3e82053db930699
Jeremy Huddleston <jshudd@gmail.com>
2021-09-09 14:51:23 -0500

    added assets and initial interface
    added assets and initial interface

--- MonkeyTap/Game1.cs
+++ MonkeyTap/Game1.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Media;
 
 namespace MonkeyTap
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        Texture2D monkey;
+        Texture2D background;
+        Texture2D logo;
+        SpriteFont font;
+        SoundEffect hit;
+        Song title;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -25,9 +35,18 @@ namespace MonkeyTap
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            monkey = Content.Load<Texture2D>("monkey");
+            background = Content.Load<Texture2D>("background");
+            logo = Content.Load<Texture2D>("logo");
+            font = Content.Load<SpriteFont>("font");
+            hit = Content.Load<SoundEffect>("hit");
+            title = Content.Load<Song>("title");
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(title);
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +64,10 @@ namespace MonkeyTap
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(monkey, Vector2.Zero, null, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
