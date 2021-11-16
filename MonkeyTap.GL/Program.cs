59a58345676f42b61bf78139fb8231bbd954c919
Jeremy Huddleston <jshudd@gmail.com>
2021-11-16 12:02:05 -0600

    Added OpenGL project to solution
    Added OpenGL project so game will run on Mac or Windows

---
+++ MonkeyTap.GL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonkeyTap.GL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
